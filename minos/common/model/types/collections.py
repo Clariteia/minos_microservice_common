4e0d8feabb5dfac8b8418801e4dc763a1e3e4a13
Sergio García Prado <sergio.garcia@clariteia.com>
2021-08-09 12:40:16 +0200

    ISSUE #484 * Add naive event history for `Observable` list.

--- minos/common/model/types/collections.py
+++ minos/common/model/types/collections.py
@@ -26,6 +26,7 @@ class ObservableList(MutableSequence, Generic[T]):
     def __init__(self, values: Iterable[T] = None):
         if values is None:
             values = tuple()
+        self._events = [("add", i, value) for i, value in enumerate(values)]
         self._data = list(values)
 
     def insert(self, index: int, value: T) -> NoReturn:
@@ -35,19 +36,28 @@ class ObservableList(MutableSequence, Generic[T]):
         :param value: TODO
         :return: TODO
         """
+        self._events.append(("add", index, value))
         self._data.insert(index, value)
 
     def __getitem__(self, item: int) -> T:
         return self._data[item]
 
     def __setitem__(self, key: int, value: T):
+        self._events.append(("update", key, value))
         self._data[key] = value
 
     def __delitem__(self, key: int) -> NoReturn:
+        self._events.append(("delete", key % len(self), None))
         del self._data[key]
 
     def __len__(self) -> int:
         return len(self._data)
+
+    def events(self):
+        return self._events
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self._data!r})"
 
 
 K = TypeVar("K")
---
+++ tests/test_common/test_model/test_types/test_collections.py
@@ -0,0 +1,44 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+import unittest
+
+from minos.common import (
+    ObservableList,
+)
+
+
+class TestObservableList(unittest.TestCase):
+    def test_events(self):
+        data = ObservableList([1, 2, 3, 4])
+        data.insert(3, 5)
+        data.pop()
+        data[0] = 32
+        del data[1]
+        observed = data.events()
+
+        expected = [
+            ("add", 0, 1),
+            ("add", 1, 2),
+            ("add", 2, 3),
+            ("add", 3, 4),
+            ("add", 3, 5),
+            ("delete", 4, None),
+            ("update", 0, 32),
+            ("delete", 1, None),
+        ]
+        self.assertEqual(expected, observed)
+        self.assertEqual([32, 3, 5], list(data))
+
+
+class TestObservableDict(unittest.TestCase):
+    def test_insert(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
